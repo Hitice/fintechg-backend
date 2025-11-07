bc5b4eb28a5801a47159398139bbd00bfd667ab1
hitice <adm.nuvem@protonmail.com>
2025-11-07 08:46:39 -0300

    feat: adiciona estrutura por features no backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { FinancasModule } from './financas/financas.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, FinancasModule],
   controllers: [AppController],
   providers: [AppService],
 })
